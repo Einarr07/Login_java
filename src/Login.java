c28e3f954650624df03fbe108ecf83072c5ca8b5
Einarr07 <aidenperse@gmail.com>
2023-02-01 22:44:20 -0500

    Intento de ingreso y agregasion del segundo frame

--- src/Login.java
+++ src/Login.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Login {
+public class Login extends pantalla{
     private JLabel usuario;
     private JTextField escribirUsuario;
     private JPanel panel1;
@@ -13,7 +13,48 @@ public class Login {
         entrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Entraste");
+                int contador = 0;
+                boolean ingreso = false;
+                String user = "mateo";
+                String contrasenia = "mateo123";
+                while(ingreso == false && contador < 3){
+                    if((user = String.valueOf(escribirUsuario)) == (contrasenia = String.valueOf(escribirContra))){
+                        ingreso = true;
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null,
+                                "El usuario o contraseña son incorrectos");
+                        contador++;
+                    }
+                }
+
+                if (ingreso == false) {
+                    JOptionPane.showMessageDialog(null,
+                            "Se a suspendido el numero de intentos permitidos");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Bienvenido al sistema");
+                    JFrame frame = new JFrame("IMC");
+                    frame.setContentPane(new pantalla().panel2);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.pack();
+                    frame.setSize(600, 400);
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                }
+            }
+        });
+
+        escribirUsuario.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                escribirUsuario.getAction();
+            }
+        });
+        escribirContra.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                escribirContra.getAction();
             }
         });
     }
@@ -27,5 +68,4 @@ public class Login {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-
 }
---
+++ src/pantalla.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class pantalla {
+    protected JPanel panel2;
+    private JMenuBar principalJMB;
+    private JMenu menuJM;
+    private JMenuItem itemJMI;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JRadioButton LBRadioButton;
+    private JRadioButton KGRadioButton;
+    private JRadioButton CMRadioButton;
+    private JRadioButton mRadioButton;
+    private JButton calcularButton;
+    private JButton editarImagenButton;
+    private JPanel panel3;
+    private JTextField suIMCEsTextField;
+
+
+}
